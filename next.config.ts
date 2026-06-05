819c62bc2c91a9876e31dcc19167a4dea067179b
JORGE ENCISO VASQUEZ <jorge.enciso@rimac.com.pe>
2026-06-05 12:32:45 -0500

    chore: setup github pages deployment with next.js static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/reto-tecnico-rimac' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
